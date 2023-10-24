fba68ad108b779934f98c34549fb44d76eedfe2e
Nikhil <Nikhil@gmail.com>
2023-10-24 17:27:26 +0530

    added companies/{{company_id}}/employees - path for creating custom APIs using @action

--- CompanyEmployeeAPI/CompanyApp/views.py
+++ CompanyEmployeeAPI/CompanyApp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
+
+from rest_framework.decorators import action
 
 from .models import *
 from .serializers import *
@@ -10,6 +13,17 @@ from .serializers import *
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+
+# companies/{{company_id}}/employees
+    @action(detail=True, methods=['GET'])
+    def employees(self, request, pk=None):
+        comp = Company.objects.get(pk=pk)
+        emp = Employee.objects.filter(company=comp)
+
+        emp_serializer = EmployeeSerializer(emp, many=True, context={'request': request})
+        return Response(emp_serializer.data)
+
 
 
 class EmployeeViewSet(viewsets.ModelViewSet):
